f7580edefee114879d15310bf102a1a798560d46
rongdanfeng <1179617519@qq.com>
2022-09-01 23:06:00 +0800

    add regist-center 配置

--- platform/registry-center/src/main/java/com.imooc/EurekaServerApplication.java
+++ platform/registry-center/src/main/java/com.imooc/EurekaServerApplication.java
@@ -1,4 +1,22 @@
 package com.imooc;
 
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <p>注册中心</p>
+ *
+ * @author : rdf
+ * @date : 2020-10-21 11:30
+ **/
+@SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaServerApplication.class)
+        .web(WebApplicationType.SERVLET)
+        .run(args);
+    }
 }
