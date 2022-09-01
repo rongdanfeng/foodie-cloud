3f0da6bd2292d43bf18d632b1d5729c6944d6854
rongdanfeng <1179617519@qq.com>
2022-09-02 06:33:59 +0800

    add foodie-cloud-domain-order-mapper

---
+++ domain/order/foodie-order-mapper/src/main/java/comm/imooc/order/mapper/OrderStatusMapper.java
@@ -0,0 +1,8 @@
+package comm.imooc.order.mapper;
+
+import com.imooc.my.mapper.MyMapper;
+import com.imooc.order.pojo.OrderStatus;
+
+
+public interface OrderStatusMapper extends MyMapper<OrderStatus> {
+}
---
+++ domain/order/foodie-order-mapper/src/main/java/comm/imooc/order/mapper/OrdersMapper.java
@@ -0,0 +1,8 @@
+package comm.imooc.order.mapper;
+
+import com.imooc.my.mapper.MyMapper;
+import com.imooc.order.pojo.Orders;
+
+
+public interface OrdersMapper extends MyMapper<Orders> {
+}
